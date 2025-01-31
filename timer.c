1a5f6cfd56f711713601f047a9361b54983bd675
luigimanzoli <96997088+luigimanzoli@users.noreply.github.com>
2025-01-31 16:07:16 -0300

    Estrutura inicial pré-import.

---
+++ timer.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/adc.h"
+#include "pico/bootrom.h"
+#include "hardware/pwm.h"
+
+//número de LEDs
+#define NUM_PIXELS 25
+
+//pino de saída
+#define OUT_PIN 7
+
+// Definição dos LEDs RGB
+#define RLED_PIN 13
+#define GLED_PIN 11
+#define BLED_PIN 12
+
+// Definição dos botões
+#define BTNA_PIN 5
+#define BTNB_PIN 6
+
+int contador = 0;
+static volatile uint32_t last_time = 0; 
+
+void init_all() {
+    gpio_init(RLED_PIN);
+    gpio_set_dir(RLED_PIN, GPIO_OUT);
+    gpio_put(RLED_PIN, 0);
+
+    gpio_init(GLED_PIN);
+    gpio_set_dir(GLED_PIN, GPIO_OUT);
+    gpio_put(GLED_PIN, 0);
+
+    gpio_init(BLED_PIN);
+    gpio_set_dir(BLED_PIN, GPIO_OUT);
+    gpio_put(BLED_PIN, 0);
+
+    gpio_init(BTNA_PIN);
+    gpio_set_dir(BTNA_PIN, GPIO_IN);
+    gpio_pull_up(BTNA_PIN);
+
+    gpio_init(BTNB_PIN);
+    gpio_set_dir(BTNB_PIN, GPIO_IN);
+    gpio_pull_up(BTNB_PIN);
+}
+
+void get_led(bool R, bool G, bool B) {
+    gpio_put(RLED_PIN, R);
+    gpio_put(GLED_PIN, G);
+    gpio_put(BLED_PIN, B);
+}
+
+// Inicializa o sistema de clock
+void inicializar_clock() {
+    bool ok = set_sys_clock_khz(100000, false);
+    if (ok) {
+        printf("Clock set to %ld\n", clock_get_hz(clk_sys));
+    } else {
+        printf("Falha ao configurar o clock\n");
+    }
+}
+
+void gpio_irq_handler(uint gpio, uint32_t events){
+
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+        if (current_time - last_time > 200000){
+            last_time = current_time;
+
+            }
+    }
+
+}
+
+// Função principal
+int main() {
+    // Inicializa clock, stdio e configurações
+    stdio_init_all();
+    init_all();
+    inicializar_clock();
+
+    printf("Sistema inicializado. Aguardando entrada...\n");
+
+    // Configuração dos botões como interrupções
+    gpio_set_irq_enabled_with_callback(BTNA_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BTNB_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    // Rotina inicial do programa para teste
+    gpio_put(RLED_PIN, 1);
+    sleep_ms(300);
+    gpio_put(RLED_PIN, 0);
+
+    while (true) {
+       for (int i = 0; i < 5; i++){
+            gpio_put(RLED_PIN, 1);
+            sleep_ms(100);
+            gpio_put(RLED_PIN, 0);
+            sleep_ms(100);
+       }
+    }
+
+    return 0;
+}
